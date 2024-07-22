63a21d5af377dc7c80d5d9afd05a74494eb50ca8
Olarotimi Adamson <olarotimiadamson@yahoo.com>
2024-07-22 11:30:20 +0000

    Event loop

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 //modules
 // const sayHi = require("./util");
 // const names = require("./first_module");
-const http_module = require("./http_module");
+// const http_module = require("./http_module");
+const { readFile } = require("fs");
 
 // console.log(module);
 // console.log(sayHi);
@@ -9,4 +10,17 @@ const http_module = require("./http_module");
 // sayHi("James");
 // sayHi(names.firstName);
 // sayHi(name.lastName);
-console.log(http_module);
+// console.log(http_module);
+
+console.log("started a task");
+
+readFile("./fs_text_async.txt", 'utf8', (err, result) => {
+    if (err) {
+        console.log(err)
+        return
+    }
+    console.log(result);
+    console.log("First task completed");
+})
+
+console.log("starting next task");
