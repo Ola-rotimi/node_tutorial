c780f4e7170bd0000c78e19989ccb3576739a776
Olarotimi Adamson <olarotimiadamson@yahoo.com>
2024-07-22 12:27:40 +0000

    updated event loop

--- event_loop.js
+++ event_loop.js
@@ -1,17 +1,36 @@
 const { readFile } = require("fs");
+const http = require("http")
 
 const eventLoop = () => {
-    console.log("started a task");
+//     console.log("started a task");
 
-readFile("./fs_text_async.txt", 'utf8', (err, result) => {
-    if (err) {
-        console.log(err)
-        return
-    }
-    console.log(result);
-    console.log("First task completed");
+// readFile("./fs_text_async.txt", 'utf8', (err, result) => {
+//     if (err) {
+//         console.log(err)
+//         return
+//     }
+//     console.log(result);
+//     console.log("First task completed");
+// })
+
+// console.log("starting next task");
+
+//using timeout
+// console.log("first");
+// setTimeout(()=>{
+//     console.log("second")
+// }, 0);
+// console.log("third")
+
+const server = http.createServer((req, res) => {
+    console.log('request event');
+    res.end("Hello World");
 })
 
-console.log("starting next task");}
+server.listen(5000, ()=>{
+    console.log('server listening on port 5000');
+})
+
+}
 
 module.exports = eventLoop;
